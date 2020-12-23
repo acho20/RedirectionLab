91db0409f7e0b1b5d0f20c1493fc893464b149c0
Alex Cho <acho20@stuy.edu>
2020-12-23 13:58:39 -0500

    created MakeStars.java

---
+++ MakeStars.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class MakeStars{
+  public static void main(String[] args){
+    Scanner scan = new Scanner(System.in);
+    Scanner scan2;
+    int x = 0;
+    while (true){
+      scan2 = new Scanner(scan.nextLine());
+      while (scan2.hasNext()){
+        x = scan2.next().length();
+        for(int i =0; i < x; i++){
+          System.out.printf("*");
+        }
+        System.out.printf(" ");
+      }
+      System.out.println();
+    }
+  }
+
+}

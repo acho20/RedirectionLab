c9ddb5d4022d133c2d97a7e6dd4e98a4549dde4e
Alex Cho <acho20@stuy.edu>
2020-12-23 14:12:25 -0500

    added pigLatinSimple()

--- PigLatin.java
+++ PigLatin.java
@@ -2,7 +2,14 @@ import java.util.Scanner;
 
 public class PigLatin{
   public static String pigLatinSimple(String s){
-    
+    s = s.toLowerCase();
+    if(s.charAt(0) == 'a' || s.charAt(0) == 'e' || s.charAt(0) == 'i' || s.charAt(0) == 'o' || s.charAt(0) == 'u'){
+      s += "hay";
+    }else{
+      s += s.charAt(0);
+      s = s.substring(1);
+    }
+    return s;
   }
 
 }
